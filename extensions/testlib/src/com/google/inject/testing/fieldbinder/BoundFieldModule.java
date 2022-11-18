7ee9182a5d20c2763d7e3ab8bdaeb12e21af06b9
Codescene4 <117838297+Codescene4@users.noreply.github.com>
2022-11-18 14:40:56 +0100

    Update BoundFieldModule.java

--- extensions/testlib/src/com/google/inject/testing/fieldbinder/BoundFieldModule.java
+++ extensions/testlib/src/com/google/inject/testing/fieldbinder/BoundFieldModule.java
@@ -118,6 +118,30 @@ public final class BoundFieldModule implements Module {
     boundFields = findBindableFields(deferredErrors);
     deferredBindingErrors = deferredErrors.build();
   }
+   @SuppressWarnings("unchecked")
+  private static <T> void mapModuleTest(
+      Key<T> mapKey,
+      TypeLiteral<?> keyType,
+      TypeLiteral<?> valueType,
+      Iterable<? extends Module> modules,
+      boolean allowDuplicates,
+      int expectedMapBindings,
+      MapResult<?, ?>... results) {
+    Set<Element> elements = ImmutableSet.copyOf(Elements.getElements(modules));
+    Visitor<T> visitor = new Visitor<>();
+    MapBinderBinding<T> mapbinder = null;
+    Map<Key<?>, Binding<?>> keyMap = Maps.newHashMap();
+    for (Element element : elements) {
+      if (element instanceof Binding) {
+        Binding<?> binding = (Binding<?>) element;
+        keyMap.put(binding.getKey(), binding);
+        if (binding.getKey().equals(mapKey)) {
+          mapbinder = (MapBinderBinding<T>) ((Binding<T>) binding).acceptTargetVisitor(visitor);
+        }
+      }
+    }
+    assertNotNull(mapbinder);
+
 
   /**
    * Create a BoundFieldModule which binds the {@link Bind} annotated fields of {@code instance}.
